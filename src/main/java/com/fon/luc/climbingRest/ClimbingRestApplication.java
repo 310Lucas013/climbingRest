f50171a7ba35477a7ee6ea381e56d3cfdac6d350
Luuk Vermeer <l.vermeer@student.fontys.nl>
2019-12-02 09:47:25 +0100

    First Commit.

---
+++ src/main/java/com/fon/luc/climbingRest/ClimbingRestApplication.java
@@ -0,0 +1,13 @@
+package com.fon.luc.climbingRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fon/luc/climbingRest/ClimbingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fon.luc.climbingRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
